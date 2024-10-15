37a47aefb1763a1e2cd8558536c6feb0070df177
DevMohi <morphex72@gmail.com>
2024-10-15 21:28:45 +0600

    push

---
+++ Module 17 - Bellman Ford and Floyd Warshell/check_negative_cycle.cpp
@@ -0,0 +1,88 @@
+//Extra ekta iteration korle buja jai negative cycle ase naki 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Edge{
+    public: 
+        int u;
+        int v;
+        int w; 
+        Edge(int u, int v, int w){
+            this -> u = u;
+            this -> v = v;
+            this -> w = w;
+        }
+};
+
+int main(){
+    int n,e;
+    vector<Edge> v;
+    cin>>n>>e;
+
+    while(e--){
+        int a,b,w;
+        cin>>a>>b>>w;
+        Edge ed(a,b,w);
+        // cout<<ed.u<<" "<<ed.v<<" "<<ed.w<<endl; 
+        v.push_back(ed);
+    }
+    //Setting infinity 
+    int dis[n+1];
+    for(int i=1; i<=n; i++){
+        dis[i] = INT_MAX; 
+    }
+    //Source er value ta 0 hoite hobe
+    dis[1] = 0; 
+
+    for(int i=1; i<=n-1; i++){
+        for(int j=0; j<v.size();j++){
+            Edge ed = v[j]; 
+            int a = ed.u;
+            int b = ed.v;
+            int w = ed.w;
+
+            //Relaxation 
+            if(dis[a] + w < dis[b]){
+                dis[b] = dis[a] + w; 
+            }
+        }
+    }
+
+    //Arekbat check
+    bool cycle = false; 
+
+    for(int j=0; j<v.size();j++){
+            Edge ed = v[j]; 
+            int a = ed.u;
+            int b = ed.v;
+            int w = ed.w;
+
+            //Relaxation 
+            if(dis[a] + w < dis[b]){
+                cycle = true; 
+                break; 
+                dis[b] = dis[a] + w; 
+            }
+    }
+
+    if(cycle){
+        cout<<"Cycle exist"<<endl;
+    }
+    else{
+        for(int i =1; i<=n; i++){
+            cout<<"Node "<<i<<": "<<dis[i]<<endl; 
+        }
+    }
+
+
+
+    return 0;
+}
+
+
+//Input
+// 3 3
+// 1 2 2 
+// 3 1 -1
+// 2 3 -3
